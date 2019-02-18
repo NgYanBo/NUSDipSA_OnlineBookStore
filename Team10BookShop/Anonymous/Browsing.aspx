172e9f3878e38b31ead39f5a5535713969a45593
Ng Yan Bo <e0321109@u.nus.edu>
2019-02-18 10:04:20 +0800

    Initial Commit

---
+++ Team10BookShop/Anonymous/Browsing.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Browsing.aspx.cs" Inherits="Team10BookShop.Browsing" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="height: 214px">
+            <br />
+            <asp:DropDownList ID="DetailsDL" runat="server">
+                <asp:ListItem>Author</asp:ListItem>
+                <asp:ListItem>Title</asp:ListItem>
+                <asp:ListItem>ISBN</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="CategoryDL" runat="server" OnTextChanged="CategoryDL_TextChanged" DataSourceID="SqlDataSource1" DataTextField="Name" DataValueField="Name" OnSelectedIndexChanged="CategoryDL_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:MyBooks %>" SelectCommand="SELECT [Name] FROM [Category]"></asp:SqlDataSource>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="SearchTxt" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="SearchBtn" runat="server" OnClick="SearchBtn_Click" Text="Search" />
+            <%--<br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />--%>
+            <br />
+            <br />
+            <asp:Button ID="AllBooksBtn" runat="server" OnClick="AllBooksBtn_Click" Text="All Books" />
+            <asp:Panel ID="Panel1" runat="server" Height="481px" Style="padding-top:30px">
+                <asp:DataList ID="DataList1" runat="server" DataKeyField="BookID" HorizontalAlign="Center" RepeatColumns="3" RepeatDirection="Horizontal" OnItemCommand="DataList1_ItemCommand" BorderColor="Aqua" BorderWidth="0px" CellPadding="10" CellSpacing="10" GridLines="Both">
+                  <ItemTemplate>
+                        <image height="120" src="../Images/<%# Eval("ISBN") %>.jpg" width="90" />
+                        <br />
+                        BookID:
+                        <asp:Label ID="BookIDLabel" runat="server" Text='<%# Eval("BookID") %>' />
+                        <br />
+                        Title:
+                        <asp:Label ID="TitleLabel" runat="server" Text='<%# Eval("Title") %>' />
+                        <br />
+                        CategoryID:
+                        <asp:Label ID="CategoryIDLabel" runat="server" Text='<%# Eval("CategoryID") %>' />
+                        <br />
+                        ISBN:
+                        <asp:Label ID="ISBNLabel" runat="server" Text='<%# Eval("ISBN") %>' />
+                        <br />
+                        Author:
+                        <asp:Label ID="AuthorLabel" runat="server" Text='<%# Eval("Author") %>' />
+                        <br />
+                        Stock:
+                        <asp:Label ID="StockLabel" runat="server" Text='<%# Eval("Stock") %>' />
+                        <br />
+                        Price:
+                        <asp:Label ID="PriceLabel" runat="server" Text='<%# Eval("Price") %>' />
+                        <br />
+                      <asp:Button ID="DetailButton" runat="server" Text="View Detail"  CommandName="Details" />
+                        <br />
+                    </ItemTemplate>
+                    <SelectedItemStyle Font-Bold="True" Font-Italic="False" Font-Overline="False" Font-Strikeout="False" Font-Underline="False" />
+                </asp:DataList>
+            </asp:Panel>
+            <br />
+        </div>
+</asp:Content>
---
+++ Team10BookShop/Anonymous/Browsing.aspx.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Team10BookShop
+{
+    public partial class Browsing : System.Web.UI.Page
+    {
+        int catid;
+        int id;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!IsPostBack)
+            {
+                using (MyBooks mb = new MyBooks())
+                {
+                    DataList1.DataSource = mb.Books.ToList<Book>();
+                    DataList1.DataBind();
+                }
+            }
+        }
+
+        protected void CategoryDL_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+            using (MyBooks mb = new MyBooks())
+            {
+                if (DetailsDL.Text != null && SearchTxt.Text != null && CategoryDL.SelectedValue != null)
+                {
+                    switch (DetailsDL.SelectedItem.ToString())
+                    {
+                        case "Author":
+
+                            catid = mb.Categories.Where(x => x.Name == CategoryDL.SelectedValue).Select(x => x.CategoryID).First();
+                            var q = from x in mb.Books where x.Author.Contains(SearchTxt.Text) && x.CategoryID == catid select x;
+                            if (q.Count() != 0)
+                            {
+                                DataList1.DataSource = q.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        case "Title":
+                            catid = mb.Categories.Where(x => x.Name == CategoryDL.SelectedValue).Select(x => x.CategoryID).First();
+                            var q1 = from x in mb.Books where x.Title.Contains(SearchTxt.Text) && x.CategoryID == catid select x;
+                            if (q1.Count() != 0)
+                            {
+                                DataList1.DataSource = q1.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        case "ISBN":
+                            catid = mb.Categories.Where(x => x.Name == CategoryDL.SelectedValue).Select(x => x.CategoryID).First();
+                            var q2 = from x in mb.Books where x.ISBN == (SearchTxt.Text) && x.CategoryID == catid select x;
+                            if (q2.Count() != 0)
+                            {
+                                DataList1.DataSource = q2.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        default:
+                            Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            break;
+                    }
+                }
+                else if (DetailsDL.SelectedValue != null && SearchTxt.Text == null && CategoryDL.SelectedValue != null)
+                {
+                    switch (CategoryDL.SelectedItem.ToString())
+                    {
+                        case "children":
+                            catid = mb.Categories.Select(x => x.CategoryID).First();
+                            var q1 = from x in mb.Books where x.CategoryID == catid select x;
+                            if (q1.Count() != 0)
+                            {
+                                DataList1.DataSource = q1.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        case "technical":
+                            catid = mb.Categories.Select(x => x.CategoryID).First();
+                            var q = from x in mb.Books where x.CategoryID == catid select x;
+                            if (q.Count() != 0)
+                            {
+                                DataList1.DataSource = q.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        case "finance":
+                            catid = mb.Categories.Select(x => x.CategoryID).First();
+                            var q2 = from x in mb.Books where x.CategoryID == catid select x;
+                            if (q2.Count() != 0)
+                            {
+                                DataList1.DataSource = q2.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                        case "non-fiction":
+                            catid = mb.Categories.Select(x => x.CategoryID).First();
+                            var q3 = from x in mb.Books where x.CategoryID == catid select x;
+                            if (q3.Count() != 0)
+                            {
+                                DataList1.DataSource = q3.ToList();
+                                DataList1.DataBind();
+                            }
+                            else
+                            {
+                                Response.Write("<script>confirm('Record Does Not Exist !!')</script>");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        protected void CategoryDL_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //using (MyBooks mb = new MyBooks())
+            //{
+            //    int t = (int)Session["id"];
+            //    int s = DataList1.SelectedIndex;
+            //    //id = mb.Books.Where(x => x.BookID.Equals(s)).First().BookID;
+            //    t = s;
+            //    Label1.Text = t.ToString();
+            //    Session["id"] = t;
+
+            //}
+        }
+
+        protected void DetailButton_Click(object sender, EventArgs e)
+        {
+            //int t = (int)Session["id"];
+            //int s = (int)DataList1.SelectedValue;
+            //id = mb.Books.Where(x => x.BookID.Equals(s)).First().BookID;
+            //t = s;
+            //Label1.Text = s.ToString();
+            //Session["id"] = t;
+            //DataList1_ItemCommand(sender, e);
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            Console.WriteLine("test" + e.CommandName);
+
+
+            if (e.CommandName == "Details")
+            {
+                Control control;
+                control = e.Item.FindControl("BookIDLabel");
+                int bookID = Convert.ToInt32(((Label)control).Text);
+                Book b = BusinessLogic.getBookByID(bookID);
+                Session["id"] = b.BookID;
+                Response.Redirect("BookDetails.aspx");
+            }
+        }
+
+        protected void AllBooksBtn_Click(object sender, EventArgs e)
+        {
+            using (MyBooks mb = new MyBooks())
+            {
+                var q = from x in mb.Books select x;
+                DataList1.DataSource = q.ToList();
+                DataList1.DataBind();
+            }
+        }
+    }
+}
